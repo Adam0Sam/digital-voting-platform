c72cbc330c9250ff2a4452c644b102e8f196cb29
Adam Samulionis <el.naujienos@gmail.com>
2024-06-30 22:35:42 +0300

    idk

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { PrismaModule } from './prisma/prisma.module';
     }),
     ConfigModule.forRoot(),
     PrismaModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,7 +1,7 @@
 import './app.css';
 
 function App() {
-  return <h1 className="font-mono ">Hello, world!</h1>;
+  return <h1 className="font-mono">Hello, world!</h1>;
 }
 
 export default App;
