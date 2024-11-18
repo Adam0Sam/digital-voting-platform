c80d961198be3f45fbcb728c806cc56e0f7ceb35
Adam0Sam <el.naujienos@gmail.com>
2024-11-18 20:41:29 +0200

    Create UserNotification Ambassador Type

---
+++ packages/ambassador/src/notifications/notifications.ts
@@ -0,0 +1,109 @@
+import { z } from "zod";
+import { Candidate, CandidateSchema } from "../candidate/index.js";
+import { mutableProposalKeys } from "../proposal/index.js";
+import { atLeastTwo } from "../utils/index.js";
+
+export const UserNotificationType = {
+  VOTE_SUGGESTION: "VOTE_SUGGESTION",
+  PROPOSAL_RESOLUTION: "PROPOSAL_RESOLUTION",
+  PROPOSAL_ABORTION: "PROPOSAL_ABORTION",
+  PROPOSAL_ACTIVATION: "PROPOSAL_ACTIVATION",
+  PROPOSAL_UPDATE: "PROPOSAL_UPDATE",
+  VOTE_DISABLED: "VOTE_DISABLED",
+  VOTE_ENABLED: "VOTE_ENABLED",
+} as const;
+
+export const BaseUserNotificationSchema = z.object({
+  userId: z.string().uuid().optional(),
+  proposalId: z.string().uuid(),
+  createdAt: z.date(),
+  read: z.boolean(),
+});
+
+export const CreateBaseUserNotificationDtoSchema =
+  BaseUserNotificationSchema.omit({
+    read: true,
+    createdAt: true,
+  });
+
+const ProposalResolvedNotificationContentSchema = z.object({
+  type: z.literal(UserNotificationType.PROPOSAL_RESOLUTION),
+  content: z.object({
+    winningCandidate: z.array(CandidateSchema),
+    votesCount: z.number().int().positive(),
+  }),
+});
+
+const CreateProposalResolvedNotificationContentDtoSchema = z.object({
+  type: z.literal(UserNotificationType.PROPOSAL_RESOLUTION),
+  content: z.null(),
+});
+
+const genericNotificationContentSchemas = [
+  z.object({
+    type: z.literal(UserNotificationType.VOTE_SUGGESTION),
+    content: z.object({
+      suggestedBy: z.string().uuid(),
+      candidates: z.array(CandidateSchema),
+    }),
+  }),
+  z.object({
+    type: z.literal(UserNotificationType.PROPOSAL_ABORTION),
+    content: z.object({
+      reason: z.string().optional(),
+    }),
+  }),
+  z.object({
+    type: z.literal(UserNotificationType.PROPOSAL_ACTIVATION),
+    content: z.object({
+      startDate: z.date(),
+      endDate: z.date(),
+    }),
+  }),
+  z.object({
+    type: z.literal(UserNotificationType.PROPOSAL_UPDATE),
+    content: z.object({
+      updatedFields: z.array(z.enum(mutableProposalKeys)),
+      updatedValues: z.array(z.any()),
+    }),
+  }),
+  z.object({
+    type: z.literal(UserNotificationType.VOTE_DISABLED),
+    content: z.object({
+      reason: z.string().optional(),
+    }),
+  }),
+  z.object({
+    type: z.literal(UserNotificationType.VOTE_ENABLED),
+    content: z.null(),
+  }),
+];
+
+const UserNotificationContentSchema = z.discriminatedUnion("type", [
+  ...atLeastTwo([
+    ...genericNotificationContentSchemas,
+    ProposalResolvedNotificationContentSchema,
+  ]),
+]);
+
+export const UserNotificationSchema = BaseUserNotificationSchema.extend({
+  package: UserNotificationContentSchema,
+});
+
+const CreateUserNotificationContentDtoSchema = z.discriminatedUnion("type", [
+  ...atLeastTwo([
+    ...genericNotificationContentSchemas,
+    CreateProposalResolvedNotificationContentDtoSchema,
+  ]),
+]);
+export const CreateUserNotificationDtoSchema =
+  CreateBaseUserNotificationDtoSchema.extend({
+    package: CreateUserNotificationContentDtoSchema,
+  });
+
+export type UserNotification = z.infer<typeof UserNotificationSchema>;
+export type CreateUserNotificationDto = z.infer<
+  typeof CreateUserNotificationDtoSchema
+>;
+
+type a = z.infer<typeof CreateBaseUserNotificationDtoSchema>;
--- packages/ambassador/src/utils/type-validators.ts
+++ packages/ambassador/src/utils/type-validators.ts
@@ -1,28 +1,28 @@
-import { ReadonlyStringLiteralObject } from './util-types';
+import { ReadonlyStringLiteralObject } from "./util-types";
 
 export function isType<T>(
   value: unknown,
-  validationFn: (value: unknown) => boolean,
+  validationFn: (value: unknown) => boolean
 ): value is T {
   return validationFn(value);
 }
 
 export function isTypeArray<T>(
   value: unknown,
-  validationFn: (value: unknown) => value is T,
+  validationFn: (value: unknown) => value is T
 ) {
   return isType<T[]>(
     value,
-    value => Array.isArray(value) && value.every(validationFn),
+    (value) => Array.isArray(value) && value.every(validationFn)
   );
 }
 
 export function isKeyOfStringLiteralObj<T extends { [key: string]: string }>(
   item: unknown,
-  literalObj: ReadonlyStringLiteralObject<T>,
+  literalObj: ReadonlyStringLiteralObject<T>
 ) {
   const isKey = (item: unknown): item is keyof T => {
-    if (typeof item === 'string') {
+    if (typeof item === "string") {
       return Object.keys(literalObj).includes(item);
     }
     return false;
@@ -37,4 +37,12 @@ export function isKeyOfStringLiteralObjArray<
   const validatorWithLiteralObj = (item: unknown) =>
     isKeyOfStringLiteralObj(item, literalObj);
   return isTypeArray<keyof T>(item, validatorWithLiteralObj);
+}
+
+export function atLeastTwo<T>(arr: T[]): [T, T, ...T[]] {
+  if (arr.length < 2) {
+    throw new Error("Instantiated with fewer than two items");
+  }
+
+  return arr as [T, T, ...T[]];
 }
